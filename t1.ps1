5c3fe193784bb7421d9ac47561a79e76a50dd260
webbstacken <glenn.wadstedt@protonmail.com>
2024-04-18 14:21:09 +0200

    Update t1.ps1

--- t1.ps1
+++ t1.ps1
@@ -3,5 +3,9 @@
 # >powershell iex (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/webbstacken/test/main/t1.ps1')
 # Usage ps:
 # >iex (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/webbstacken/test/main/t1.ps1')
+$filename = dump.txt
 
-Get-ComputerInfo > dump.txt
+Get-ComputerInfo > $filename
+$PSVersionTable >> $filename
+echo "Get-ExecutionPolicy" >> $filename
+Get-ExecutionPolicy >> $filename
