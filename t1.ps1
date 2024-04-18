9f1a9abbc84513595851026bab8a1fc2721cbdd6
webbstacken <glenn.wadstedt@protonmail.com>
2024-04-18 14:11:28 +0200

    Update t1.ps1

--- t1.ps1
+++ t1.ps1
@@ -1,1 +1,7 @@
+# https://forums.hak5.org/topic/41729-running-powershell-in-memory/
+# Usage cmd:
+# >powershell iex (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/webbstacken/test/main/t1.ps1')
+# Usage ps:
+# >iex (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/webbstacken/test/main/t1.ps1')
+
 Get-ComputerInfo > dump.txt
